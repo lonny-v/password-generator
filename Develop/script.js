acb9ab9ef93e892087ee107bc4eaa5a492515597
Lonny Vberg <lonnyvandenberg@gmail.com>
2021-08-10 14:21:58 -0600

    valid length

--- Develop/script.js
+++ Develop/script.js
@@ -14,43 +14,49 @@ function writePassword() {
 }
 
 // Add event listener to generate button
-//generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", writePassword);
 function generatePassword() {
+  var passwordLength = parseInt(prompt("Enter valid length(11-120)"))
+  if (passwordLength < 10 || passwordLength > 120) {
+    return "Please enter valid password length between 11 and 120"
+  }
+  else {
 
-  var includeLowercase = confirm("Include Lowercase Letters?");
 
-//console.log(confirm("Include Lowercase Letters?"));
+    var includeLowercase = confirm("Include Lowercase Letters?");
 
-  var includeUppercase = confirm("Include Uppercase Letters?");
-//console.log(confirm("Include Uppercase Letters?"));
+    //console.log(confirm("Include Lowercase Letters?"));
 
-  var includeNumbers = confirm("Include Numbers?");
-//console.log(confirm("Include Numbers?"));
+    var includeUppercase = confirm("Include Uppercase Letters?");
+    //console.log(confirm("Include Uppercase Letters?"));
 
-  var includeSymbols = confirm("Include Symbols?");
-//console.log(confirm("Include Symbols?"));
+    var includeNumbers = confirm("Include Numbers?");
+    //console.log(confirm("Include Numbers?"));
 
+    var includeSymbols = confirm("Include Symbols?");
+    //console.log(confirm("Include Symbols?"));
+  }
 }
 
-function getRandLower () {
+function getRandLower() {
   const lower = 'abcdefghijklmnopqrstuvwxyz';
 
   return lower[Math.floor(Math.random() * lower.length)]
 }
 
-function getRandUpper () {
+function getRandUpper() {
   const upper = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
 
   return upper[Math.floor(Math.random() * upper.length)]
 }
 
-function getRandNumber () {
+function getRandNumber() {
   const numbers = '0123456789';
 
   return numbers[Math.floor(Math.random() * numbers.length)]
 }
 
-function getRandSymbol () {
+function getRandSymbol() {
   const symbols = '!@#$%^&*(){}[]=<>/,.';
 
   return symbols[Math.floor(Math.random() * symbols.length)]

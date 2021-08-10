b83a7f05b80f6d4a31f40bbc4f4969107d16c52a
Lonny Vberg <lonnyvandenberg@gmail.com>
2021-08-10 14:13:33 -0600

    adding var

---
+++ Develop/script.js
@@ -0,0 +1,58 @@
+// Assignment code here
+
+
+// Get references to the #generate element
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+//generateBtn.addEventListener("click", writePassword);
+function generatePassword() {
+
+  var includeLowercase = confirm("Include Lowercase Letters?");
+
+//console.log(confirm("Include Lowercase Letters?"));
+
+  var includeUppercase = confirm("Include Uppercase Letters?");
+//console.log(confirm("Include Uppercase Letters?"));
+
+  var includeNumbers = confirm("Include Numbers?");
+//console.log(confirm("Include Numbers?"));
+
+  var includeSymbols = confirm("Include Symbols?");
+//console.log(confirm("Include Symbols?"));
+
+}
+
+function getRandLower () {
+  const lower = 'abcdefghijklmnopqrstuvwxyz';
+
+  return lower[Math.floor(Math.random() * lower.length)]
+}
+
+function getRandUpper () {
+  const upper = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+
+  return upper[Math.floor(Math.random() * upper.length)]
+}
+
+function getRandNumber () {
+  const numbers = '0123456789';
+
+  return numbers[Math.floor(Math.random() * numbers.length)]
+}
+
+function getRandSymbol () {
+  const symbols = '!@#$%^&*(){}[]=<>/,.';
+
+  return symbols[Math.floor(Math.random() * symbols.length)]
+}
+console.log(getRandSymbol())
